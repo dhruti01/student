dd43d74da45c9f55e971356b85d12e7cf3c9d3a7
dhruv <dhruv@LAPTOP-NCRGB533>
2020-01-14 16:52:06 -0500

    first commit

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -0,0 +1,27 @@
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author sidhu376
+ */
+public class StudentList {
+    
+    public static void main(String[] args) {
+        
+        Student [] sList= new Student[5];
+        
+        for (int i=0;i<sList.length;i++)
+        {
+            sList[i]=new Student();
+            sList[i].setName("Student" +(i+1));
+        }
+        
+        for (int i=0;i<sList.length;i++)
+        {
+            System.out.println(sList[i].getName());
+          
+        }
+        
+    }
+    
+}
